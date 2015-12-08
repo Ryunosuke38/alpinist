b73da7861deb29ce6038bd7e12f2f034f71c897d
Tim Riley <tim@openmonkey.com>
2015-12-08 21:21:56 +1100

    Use i18n for dry-validation

--- apps/main/lib/main/validation/user_form_schema.rb
+++ apps/main/lib/main/validation/user_form_schema.rb
@@ -4,6 +4,8 @@ require "dry/validation/schema/form"
 module Main
   module Validation
     class UserFormSchema < Dry::Validation::Schema::Form
+      configure { config.messages = :i18n }
+
       key(:name) { |name| name.filled? }
       key(:email) { |email| email.filled? }
       confirmation(:password)
