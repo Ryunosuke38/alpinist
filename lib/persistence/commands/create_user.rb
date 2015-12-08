e13ffebe06cf5f498089ea27bb0b4dfdf3b7cc39
Tim Riley <tim@openmonkey.com>
2015-12-08 11:53:31 +1100

    Use dry-data schemas as ROM command inputs

--- lib/persistence/commands/create_user.rb
+++ lib/persistence/commands/create_user.rb
@@ -1,6 +1,14 @@
+require "dry-data"
+
 module Persistence
   module Commands
     class CreateUser < ROM::Commands::Create[:sql]
+      input Dry::Data["hash"].schema(
+        name: "string",
+        email: "string",
+        encrypted_password: "string",
+      )
+
       relation :users
       register_as :create
       result :one
--- lib/persistence/commands/update_user.rb
+++ lib/persistence/commands/update_user.rb
@@ -1,6 +1,14 @@
+require "dry-data"
+
 module Persistence
   module Commands
     class UpdateUser < ROM::Commands::Update[:sql]
+      input Dry::Data["hash"].schema(
+        name: "string",
+        email: "string",
+        encrypted_password: "string",
+      )
+
       relation :users
       register_as :update
       result :one
