f0189ecd03fd56e7acb09bcee1cdd009ede86575
Tim Riley <tim@openmonkey.com>
2015-12-30 18:08:19 +1100

    Require an authorized user to view the users CRUD section

--- apps/main/core/main/container.rb
+++ apps/main/core/main/container.rb
@@ -2,6 +2,7 @@ module Main
   class Container < Rodakase::Container
     setting :root, Pathname(__FILE__).join("../..").realpath.dirname.freeze
     setting :auto_register, %w(
+      lib/main/authentication
       lib/main/operations
       lib/main/sessions
       lib/main/validation
--- apps/main/web/routes/users.rb
+++ apps/main/web/routes/users.rb
@@ -1,49 +1,57 @@
 module Main
   class Application < Rodakase::Application
     route "users" do |r|
-      r.on "new" do
-        r.is to: "main.views.users.new"
-      end
+      r.resolve "main.authentication.authorize" do |authorize|
+        authorize.(session) do |user|
+          if !user
+            r.redirect "/sign_in"
+          end
 
-      r.on ":id" do |user_id|
-        r.on "edit" do
-          r.is to: "main.views.users.edit", call_with: [id: user_id]
-        end
+          r.on "new" do
+            r.is to: "main.views.users.new"
+          end
 
-        r.post do
-          r.resolve "main.operations.update_user" do |update_user|
-            update_user.(user_id, r[:user]).match do |m|
-              m.success do
-                r.redirect "/users"
-              end
+          r.on ":id" do |user_id|
+            r.on "edit" do
+              r.is to: "main.views.users.edit", call_with: [id: user_id]
+            end
 
-              m.failure do |errors|
-                r.resolve "main.views.users.edit" do |view|
-                  view.(id: user_id, params: r[:user], errors: errors)
+            r.post do
+              r.resolve "main.operations.update_user" do |update_user|
+                update_user.(user_id, r[:user]).match do |m|
+                  m.success do
+                    r.redirect "/users"
+                  end
+
+                  m.failure do |errors|
+                    r.resolve "main.views.users.edit" do |view|
+                      view.(id: user_id, params: r[:user], errors: errors)
+                    end
+                  end
                 end
               end
             end
           end
-        end
-      end
 
-      r.post do
-        r.resolve "main.operations.create_user" do |create_user|
-          create_user.(r[:user]).match do |m|
-            m.success do
-              r.redirect "/users"
-            end
+          r.post do
+            r.resolve "main.operations.create_user" do |create_user|
+              create_user.(r[:user]).match do |m|
+                m.success do
+                  r.redirect "/users"
+                end
 
-            m.failure do |errors|
-              r.resolve "main.views.users.new" do |view|
-                view.(params: r[:user], errors: errors)
+                m.failure do |errors|
+                  r.resolve "main.views.users.new" do |view|
+                    view.(params: r[:user], errors: errors)
+                  end
+                end
               end
             end
           end
+
+          r.is to: "main.views.users.index"
         end
       end
-
-      r.is to: "main.views.users.index"
     end
   end
 end
